767a04ff408e32c93b804a6c7fe400c4b87cb819
junweizhang <1039830064@qq.com>
2015-12-07 16:29:58 +0800

    the binarydata code and document

---
+++ Databinary.R
@@ -0,0 +1,105 @@
+library(rms)
+Data.binary<-function(data,form){
+  
+ division.form<-function(form){
+   team<-terms(form)
+   chrlist<-attr(team,"term.labels")
+   chrlist
+ }
+
+ cal.auc<-function(mydata){
+  data<-mydata[order(mydata$P,decreasing=F),]#以概率P从小到大进行排序
+  len.1<-length(which(data$Y==1))
+  
+  len.0<-length(data$Y)-len.1
+  
+  xstep<-1.0/len.0
+  ystep<-1.0/len.1
+  max<--10.0
+  max.index<-0
+  x<-1.0
+  y<-1.0
+  label<-data$Y
+  xvalue<-data$X
+  temp<-0
+  for(i in 1:length(data$Y)){
+    if(label[i]!=1){
+      x<-x-xstep
+      y<-y
+      if((y-x)>0)
+        temp<-y-x
+      else
+        temp<-x-y
+      len<-temp
+      if(len>max){
+        max.index<-i
+        max<-len
+        
+      }
+    }
+    else{
+      x<-x
+      y<-y-ystep
+      if((y-x)>0)
+        temp<-y-x
+      else
+        temp<-x-y
+      len<-temp
+      if(len>max){
+        max.index=i
+        max=len
+      }
+    }
+  }
+   return (xvalue[max.index])
+ }
+
+ cal.binary<-function(mydata,thre,xname){
+   len<-nrow(mydata)
+   for(i in 1:len){
+    if(mydata[i,xname]>thre){
+      if(xname=='X10'||xname=='X13'){
+        mydata[i,xname]<-0
+      }
+      else{
+        mydata[i,xname]<-1
+      }
+      
+    }
+    
+    else{
+      if(xname=='X10'||xname=='X13'){
+        mydata[i,xname]<-1
+      }
+      else{
+        mydata[i,xname]<-0
+      }
+    }
+  }
+  mydata
+ }
+
+ cal.binaryAll<-function(mydata,form){
+  list.x<-division.form(form)
+  len<-length(list.x)
+  for(i in 1:len){
+    x<-list.x[i]
+    
+    if(length(unique(mydata[,x]))!=2){
+      data1<-mydata[,c(x,'Y')]
+      names(data1)[1]<-"X"
+      fit<-lrm(Y~X,x=TRUE, y=TRUE, data = data1)
+      pred<-predict(fit,data1)
+      data1$P=pred
+      thre<-cal.auc(data1)
+      mydata<-cal.binary(mydata,thre,x)
+    }
+   }
+  mydata
+ }
+ cal.binaryAll(data,form)
+}
+
+form=Y~X1+X2+X3+X4+X7+X10+X11+X12+X13+X14+X15
+mydata<-read.table(file='originaldata.csv',header=TRUE,sep=",")
+Data.binary(mydata,form)
---
+++ README.txt
@@ -0,0 +1,17 @@
+#################
+
+Auto Binary Data
+Databinary.R code file
+Data.binary(data,form)
+
+Arguments:
+
+	data           the input data
+	form           a formula object.An offset term can be included.The offset causes fitting of a model 
+                       such as logit(Y=1)=X¦Â+W,where W is the offset variable having no estimated coefficient.
+	               The response variable can be any data type.
+        
+
+Value
+
+        binarydata     already binary data
